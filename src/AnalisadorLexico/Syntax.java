a82b93987a59167bce290b6e4a25b187b1884c8e
João Vinicius <joao.vinicius2008@hotmail.com>
2018-08-30 19:25:47 -0300

    ADD: Assinatura de métodos

--- src/AnalisadorLexico/Syntax.java
+++ src/AnalisadorLexico/Syntax.java
@@ -7,14 +7,27 @@ public class Syntax {
    private Table currentToken;
    private int nextTokenIndex;
    
-   private void nextToken(){
-       this.currentToken = tokens.get(nextTokenIndex++);
-   }
-   
    public Syntax(ArrayList<Table> tokens){
        this.tokens = tokens;
        this.nextTokenIndex = 0;
-       nextToken();
+   }
+   
+   private void previousToken(){
+       
+       if(nextTokenIndex - 1 >= 0)
+           this.currentToken = tokens.get(--nextTokenIndex);
+       else
+           System.out.println("Tem como voltar n, chefe.");
+   }
+   
+   private void nextToken(){
+        if(nextTokenIndex + 1 < tokens.size())
+            this.currentToken = tokens.get(nextTokenIndex++);
+        else
+            System.out.println("É só isso\n"
+                            + "não tem mais jeito\n"
+                            + "Acabou\n"
+                            + "Boa sorte");
    }
    
    public void execute(){
@@ -22,20 +35,128 @@ public class Syntax {
        if(this.tokens == null)
            System.exit(0);
        
-       if(currentToken.getClassificacao().equals("Palavra reservada")
-       && currentToken.getToken().equals("Program")){
+       nextToken();
+       
+       if(currentToken.getToken().equals("Program")){
            
            nextToken();
            
            if(currentToken.getClassificacao().equals("Identificador")){
                nextToken();
                
-               if(currentToken.getClassificacao().equals("Delimitador")
-               && currentToken.getToken().equals(";")){
+               if(currentToken.getToken().equals(";")){
                    nextToken();
+                   
+                   
                }
                
            }
        }
+   }
+   
+   public void declaracaoVariaveis(){
+       
+       if(this.currentToken.getToken().equals("var")){
+           nextToken();
+           listaDeclaracaoVariaveis();
+       }
+       
+   }
+   
+   public void listaDeclaracaoVariaveis(){
+       
+   }
+   
+   public void listaIdentificadores(){
+       
+   }
+   
+   public void tipo(){
+       
+   }
+   
+   public void declaracaoSubprogramas(){
+       
+   }
+   
+   public void declaracaoSubprograma(){
+       
+   }
+   
+   public void argumentos(){
+       
+   }
+   
+   public void listaParametros(){
+       
+   }
+   
+   public void comandoComposto(){
+       
+   }
+   
+   public void comandosOpcionais(){
+       
+       
+   }
+   
+   public void listaComandos(){
+       
+   }
+   
+   public void comando(){
+       
+       
+   }
+   
+   public void parteElse(){
+       
+   }
+   
+   public void variavel(){
+       
+   }
+   
+   public void ativacaoProcedimento(){
+       
+   }
+   
+   public void listaExpressoes(){
+       
+   }
+   
+   public void expressao(){
+       
+   }
+   
+   public void expressaoSimples(){
+       
+   }
+   
+   public void termo(){
+       
+   }
+   
+   public void fator(){
+       
+       
+   }
+   
+   public void sinal(){
+       
+       
+   }
+   
+   public void opRelacional(){
+       
+   }
+   
+   public void opAditivo(){
+       
+       
+   }
+   
+   public void opMultiplicativo(){
+       
    }
 }
